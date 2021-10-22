7c2c52ca8edb148d6d78fe3c8aee8ccad497bad7
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2021-10-22 19:17:41 -0400

    rename variables and add comments

--- Combine.py
+++ Combine.py
@@ -5,32 +5,35 @@ from os.path import isfile, join
 #gets the pdfs from the files folder
 pdfs = listdir('files')
 
+#prints the unsorted array
 print("Unsorted: ")
 print(pdfs)
 
 #gets the numbers at the start of the title with its original array index
 numbers = []
 for i in range(0, len(pdfs)):
-    temp = "";
+    tempNumber = "";
     for d in range(0, len(pdfs[i])):
+        #if the character is not a digit 0 to 9 break 
         if(ord(pdfs[i][d]) < 48 or ord(pdfs[i][d]) > 57):
             break
-        temp += pdfs[i][d]
-    numbers.append([int(temp), i])
+        tempNumber += pdfs[i][d]
+    numbers.append([int(tempNumber), i])
 
 #sorts the numbers
 for lI in range(1, len(numbers)):
     for blI in range(lI-1, -1, -1):
         if numbers[blI + 1][0] < numbers[blI][0]:
-            temp = numbers[blI + 1]
+            holder = numbers[blI + 1]
             numbers[blI + 1] = numbers[blI]
-            numbers[blI] = temp
+            numbers[blI] = holder
 
 #using the indexes saved in the sorted "numbers" to create an array with the sorted file names
 doc = []
 for i in range(0, len(numbers)):
     doc.append(pdfs[numbers[i][1]])
 
+#prints the sorted array
 print("Sorted: ")
 print(doc)
 
